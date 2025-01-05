747968596a8eb5aa2c2a36c8e262956c880a975d
kirito <manhtruong@gmail.com>
2025-01-06 00:39:31 +0700

    update code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+// rafce :phím tắt để tạo mới một component
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home flex container">
+      <div className="mainText">
+        <h1>Share pixel</h1>
+      </div>
+      <div className="row row--dense">
+        <div className="col col-6">left</div>
+        <div className="col col-6">right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
